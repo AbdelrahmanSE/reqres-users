8c430660ca773ed8cce862c858a0837c3cae349c
Abdelrahman ElNaggar <abdelrahmanse@hotmail.com>
2018-12-22 21:49:54 +0200

    Finished the User header

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -5,12 +5,13 @@ import { SingleComponent } from './single/single.component';
 import { AddComponent } from './add/add.component';
 import { EditComponent } from './edit/edit.component';
 import { DeleteComponent } from './delete/delete.component';
+import { UsersHeaderComponent } from './users-header/users-header.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ListComponent, SingleComponent, AddComponent, EditComponent, DeleteComponent],
+  declarations: [ListComponent, SingleComponent, AddComponent, EditComponent, DeleteComponent, UsersHeaderComponent],
   exports: [ListComponent, SingleComponent, AddComponent, EditComponent, DeleteComponent]
 })
 export class UsersModule { }
