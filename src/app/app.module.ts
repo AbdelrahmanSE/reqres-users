0f629f2943450ba9cb283e55cbf3ba884dff064f
Abdelrahman ElNaggar <abdelrahmanse@hotmail.com>
2018-12-22 21:29:38 +0200

    Designed the Login Component Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { UsersModule } from './users/users.module';
 import { SharedModule } from './shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
