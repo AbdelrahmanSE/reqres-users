18e432d2229dbda4bf66725c199f8a2afe8fdee5
Abdelrahman ElNaggar <abdelrahmanse@hotmail.com>
2018-12-23 02:34:49 +0200

    Created the edit and delete buttons as expanded versions added new favicon

--- src/app/users/edit/edit.component.ts
+++ src/app/users/edit/edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-edit',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.scss']
 })
 export class EditComponent implements OnInit {
+  @Input()
+  btnType = 'collapsed';
 
   constructor() { }
 
