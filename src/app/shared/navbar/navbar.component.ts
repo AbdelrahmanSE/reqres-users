fd79b39c18ab49b61085015da69ba16817537679
Abdelrahman ElNaggar <abdelrahmanse@hotmail.com>
2019-02-03 00:59:06 +0200

    Version Upgrade

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -11,6 +11,10 @@ export class NavbarComponent implements OnInit {
     ngOnInit() {
     }
 
+    isLogged() {
+        return this.authService.isLogged();
+    }
+
     /**
      * Logout on Click
      */
